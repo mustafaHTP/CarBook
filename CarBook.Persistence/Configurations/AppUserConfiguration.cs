f4ffe1c5206c3e2c1a439c86a324660a193c0ca8
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2025-03-05 22:49:39 +0300

    refactor: navigation and update user management
    - Updated breadcrumbs in `Index.cshtml` to use ASP.NET Core tag helpers for improved navigation.
    - Changed hero section background image from `bg_3.jpg` to `bg_c.png`.
    - Replaced static rent button with dynamic links to car details using tag helpers.
    - Added `AppUserConfiguration` class for seeding default admin and user accounts in the database.

---
+++ CarBook.Persistence/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Persistence.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            //Create default admin and normal user
+            builder.HasData(
+                new { Id = 1, FirstName = "Admin", LastName = "Admin", Email = "admin@mail.com", AppUserRole = Domain.Enums.AppUserRole.Admin, Password = "admin123"},
+                new { Id = 2, FirstName = "User", LastName = "User", Email = "user@mail.com", AppUserRole = Domain.Enums.AppUserRole.User, Password = "user123"});
+        }
+    }
+}
