45a5a22480b4214f71abd4e50a28f19405262f66
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2025-03-23 19:08:41 +0300

    update: IconUrl property in GetServiceByIdDto

--- CarBook.Application/Dtos/ServiceDtos/GetServiceByIdDto.cs
+++ CarBook.Application/Dtos/ServiceDtos/GetServiceByIdDto.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string IconUrl { get; set; }
+        public string IconUrl { get; set; } = null!;
     }
 }
